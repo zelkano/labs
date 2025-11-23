3f1dd8e80bc6bbf6c2a0f85658a8753cc9d18e1f
zelkano <tha_kleric@hotmail.co.uk>
2025-11-23 10:08:16 -0600

    searches identify

--- api.py
+++ api.py
@@ -14,13 +14,15 @@ f5_authheader = {'Content-Type': 'application/json','Authorization' : basic}
 
 ## connect and get the Pools
 
+print("\nGathering Pools and Pool Members\n")
+
 outpool = requests.get("https://bigip-b.example.net/mgmt/tm/gtm/pool/a/",headers=f5_authheader,verify=False)
 jspool = outpool.json()
 pools = json.dumps(jspool, indent=4)
 pool = jspool['items']
 
 for t in pool:
-    print(t['name'])
+    #print(t['name'])
     outmems = requests.get(f"https://bigip-b.example.net/mgmt/tm/gtm/pool/a/~Common~{t['name']}/members",headers=f5_authheader,verify=False)
     jsmems = outmems.json()
     membs = json.dumps(jsmems, indent=4)
@@ -30,13 +32,14 @@ for t in pool:
 
     for xy in members:
         memname = xy['name']
-        print(f'Name: {memname}')
+        #print(f'Name: {memname}')
         memlist.append(memname)
     
 
     poolandmember[t['name']] = {"Members" : memlist}
 
-
+print("done!\n")
+""" 
 #print(pools)
 
 print("\n\n---\n")
@@ -45,9 +48,10 @@ for i in poolandmember:
 
     print(poolandmember[i]['Members'][0])
 
-
+"""
 
 ## connect and get the virtual servers
+print("\nGathering GTM Servers and Virtual Servers\n")
 
 output = requests.get("https://bigip-b.example.net/mgmt/tm/gtm/server",headers=f5_authheader,verify=False)
 outs = output.json()
@@ -56,17 +60,19 @@ servers = json.dumps(outs, indent=4)
 ## count how many GTM servers there are
 numserv = len(outs['items'])
 
-print(f'there are {numserv} Servers found on the GTM')
+#print(f'there are {numserv} Servers found on the GTM')
 
 
 for x in outs['items']:
     servName = x['name']
     servProduct = x['product']
     a23 = x['datacenter']
-    servDC = a23.strip('/Common/')
+    servDC = a23.lstrip('/')
+    servDC = servDC.lstrip('Common')
+    servDC = servDC.lstrip('/')
 
-    print("-----------------------------\n")
-    print(f"NAME:    {servName} \nPRODUCT: {servProduct} \nDC:      {servDC}")
+    #print("-----------------------------\n")
+    #print(f"NAME:    {servName} \nPRODUCT: {servProduct} \nDC:      {servDC}")
 
     
     ### conenct to the GTM Server and list out its Virtual Servers
@@ -76,7 +82,7 @@ for x in outs['items']:
 
     numvs = len(outsvs['items'])
 
-    print(f'\nthere are {numvs} Servers found on {servName}\n')
+    #print(f'\nthere are {numvs} Virtual Servers found on {servName}\n')
 
     
     servlist=[]
@@ -94,7 +100,7 @@ for x in outs['items']:
             vsMon = vsMon.lstrip('/')
 
 
-        print(f'Name: {vsName}:{vsDest}\nMonitor: {vsMon}')
+        #print(f'Name: {vsName}:{vsDest}\nMonitor: {vsMon}')
         servlist.append(vsName)
 
     serverandvirtual[servName] = {'Members' : servlist}
@@ -102,25 +108,30 @@ for x in outs['items']:
     #print(vslist)
 
 print("-----------------------------\n")
+print("searching for Virtual Servers which do not exist in Pools\n")
 
 for i in serverandvirtual:
     #print(f"{i} -- {serverandvirtual[i]['Members']}")
     
     for x in serverandvirtual[i]['Members']:
         found = False
-        print(f"searchign for {x}")
+        #print(f"searchign for {x}")
 
         for k in poolandmember:
             #print(f"{i} -- {poolandmember[i]['Members']}")
+
             for y in poolandmember[k]['Members']:
-                if x in y:
-                    print(f"Server found in pool {k}")
-                    found = True
+                if found == True:
                     break
-        
+                else:
+                    if x in y:
+                        #print(f"Server found in pool {k}")
+                        found = True
+                        break
+            
 
         if found == False:
-            print(f"Virtual Server {x} - not found in any pools")
+            print(f"Virtual Server {i}:{x} - not found in any pools")
 
     
 quit()

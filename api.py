f128b855e4a961715ba6e5d7a9f3bf44df428a83
zelkano <tha_kleric@hotmail.co.uk>
2025-11-23 09:07:39 -0600

    Update api.py

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 exec(open('0_global_vars.py').read())
 
+poolandmember={}
+
 uzn = input("Enter Username:")
 paz = getpass.getpass("Enter Password:")
 
@@ -8,6 +10,44 @@ b64_auth = base64.b64encode(auth_to_encode.encode()).decode()
 basic = f"Basic {b64_auth}"
 
 f5_authheader = {'Content-Type': 'application/json','Authorization' : basic}
+
+## connect and get the Pools
+
+outpool = requests.get("https://bigip-b.example.net/mgmt/tm/gtm/pool/a/",headers=f5_authheader,verify=False)
+jspool = outpool.json()
+pools = json.dumps(jspool, indent=4)
+pool = jspool['items']
+
+for t in pool:
+    print(t['name'])
+    outmems = requests.get(f"https://bigip-b.example.net/mgmt/tm/gtm/pool/a/~Common~{t['name']}/members",headers=f5_authheader,verify=False)
+    jsmems = outmems.json()
+    membs = json.dumps(jsmems, indent=4)
+    members = jsmems['items']
+
+    memlist = []
+
+    for xy in members:
+        memname = xy['name']
+        print(f'Name: {memname}')
+        memlist.append(memname)
+    #print(f'{t['name']} - {t['membersReference']['link']}')
+
+    poolandmember[t['name']] = {"Members" : memlist}
+
+
+#print(pools)
+
+print("\n\n---\n")
+
+for i in poolandmember:
+    print(poolandmember[i]['Members'][0])
+
+
+#print(poolandmember['B_pool_mixed'][0])
+
+quit()
+
 
 ## connect and get the virtual servers
 
